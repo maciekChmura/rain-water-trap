03a6fbe7501119823c46e020bf4afee18db55d4e
Maciek Chmura <chmura.m@gmail.com>
2017-12-22 11:33:08 +0100

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function trapRainWater(elevationMap) {
-    // your solution here
-}
+  let pattern = /\g(w+)\g/g;
+  let foundPatterns = [];
+  let result = 0;
+  let trapHeight = Math.max(...elevationMap);
+  for (let i = 0; i < trapHeight; i++) {
+    let level = [];
+    elevationMap.forEach(element => {
+      if (element > i) {
+        level.push("gg");
+      } else {
+        level.push("w");
+      }
+    });
+    let temp = level.join("").match(pattern);
+    foundPatterns.push(...(temp || ""));
+  }
+  foundPatterns.forEach(element => {
+    let temp = element.length - 2;
+    result += temp;
+  });
+  return result;
+};
